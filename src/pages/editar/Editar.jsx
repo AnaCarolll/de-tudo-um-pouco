a4ebaceb13a455fd97891e28bfe9f680608df0f5
Ana Carol <progamacaocarol2021@gmail.com>
2023-01-13 10:12:16 -0300

    pequenas atualizações

--- src/pages/editar/Editar.jsx
+++ src/pages/editar/Editar.jsx
@@ -1,15 +1,10 @@
-import estilos from '../../pages/editar/Editar.css'
-
 function Editar(){
     return(
-    <div >
-        <form className={estilos}>
-        <div className='imagem'></div>
+        <form  className="editaProduto"  action="">
+            <div>
 
+            </div>
         </form>
-    </div>
-
-    );
-};
-
+    )
+}
 export default Editar;
